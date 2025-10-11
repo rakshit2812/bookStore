26c51be193e36aa72f7e70854e9e221e0e2d6198
Rakshit Gupta <rakshitgupta281202@gmail.com>
2025-10-11 12:55:30 +0530

    Bug fixes

--- Backend/service/Auth.js
+++ Backend/service/Auth.js
@@ -1,8 +1,13 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
-const SignatureKey = "Rakshit#@28!5%";
+const SignatureKey = process.env.JWT_KEY;
 
 export const setUser = (user) => {
+    if (!SignatureKey) {
+      throw new Error("JWT_KEY is not defined in environment variables!7");
+    }
     const TokenCode = jwt.sign({
         _id : user._id,
         fullname : user.fullname,
@@ -13,11 +18,17 @@ export const setUser = (user) => {
 }
 
 export const getUser = (token) => {
+    if (!SignatureKey) {
+      throw new Error("JWT_KEY is not defined in environment variables!20");
+    }
     if(!token){
         console.log("token not found");
         return null;
     }
     try {
+        if (!SignatureKey) {
+          throw new Error("JWT_KEY is not defined in environment variables!28");
+        }
         return jwt.verify(token , SignatureKey,)
     } catch (error) {
         console.log("Error encountered in getting user", error);
